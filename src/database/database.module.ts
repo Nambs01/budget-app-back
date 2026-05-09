87202bea9d8cf7d1a2ac89d5814f178101c66438
angelo <angelonambs@gmail.com>
2026-05-10 02:41:53 +0300

    style: format imports in DatabaseModule for better readability

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -3,21 +3,21 @@ import { ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRootAsync({
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: configService.get<string>('database.type') as any,
-        host: configService.get<string>('database.host'),
-        port: configService.get<number>('database.port'),
-        username: configService.get<string>('database.user'),
-        password: configService.get<string>('database.password'),
-        database: configService.get<string>('database.name'),
-        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-        autoLoadEntities: true,
-        synchronize: true,
-      }),
-    }),
-  ],
+    imports: [
+        TypeOrmModule.forRootAsync({
+            inject: [ConfigService],
+            useFactory: (configService: ConfigService) => ({
+                type: configService.get<string>('database.type') as any,
+                host: configService.get<string>('database.host'),
+                port: configService.get<number>('database.port'),
+                username: configService.get<string>('database.user'),
+                password: configService.get<string>('database.password'),
+                database: configService.get<string>('database.name'),
+                entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+                autoLoadEntities: true,
+                synchronize: true,
+            }),
+        }),
+    ],
 })
 export class DatabaseModule {}
