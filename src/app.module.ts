75419d8d2cac2795415083a80ff7f2baed058264
angelo <angelonambs@gmail.com>
2026-05-10 23:42:38 +0300

    feat: implement AuthGuard with public route handling and cookie-based authentication; add tests and public decorator

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,16 @@ import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { DatabaseModule } from './database/database.module';
 import { ConfModule } from './conf/conf.module';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from './guards/auth.guard';
 
 @Module({
+    providers: [
+        {
+            provide: APP_GUARD,
+            useClass: AuthGuard,
+        },
+    ],
     imports: [AuthModule, UsersModule, DatabaseModule, ConfModule],
 })
 export class AppModule {}
--- src/guards/auth.guard.ts
+++ src/guards/auth.guard.ts
@@ -1,18 +1,32 @@
 import { COOKIE_KEYS } from '@app/constants/cookie.constant';
 import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
 import { Observable } from 'rxjs';
-import { AuthService } from './auth.service';
+import { AuthService } from '../modules/auth/auth.service';
 import type { Request } from 'express';
+import { Reflector } from '@nestjs/core';
+import { IS_PUBLIC_KEY } from '@app/decorators/public.docorator';
 
 export type RequestWithUser = Request & { userId: number };
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-    constructor(private readonly authService: AuthService) {}
+    constructor(
+        private readonly authService: AuthService,
+        private readonly reflector: Reflector,
+    ) {}
 
     canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+        const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+            context.getHandler(),
+            context.getClass(),
+        ]);
+
+        if (isPublic) {
+            return true;
+        }
+
         const request = context.switchToHttp().getRequest<RequestWithUser>();
-        const accessToken = request.cookies[COOKIE_KEYS.ACCESS_TOKEN];
+        const accessToken = request.cookies[COOKIE_KEYS.ACCESS_TOKEN] as string;
 
         if (!accessToken) throw new UnauthorizedException('Aucun jeton trouvé');
 
